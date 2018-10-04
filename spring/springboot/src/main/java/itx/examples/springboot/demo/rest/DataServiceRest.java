ec5bdd5e7054dac4217642bf038016b82cc1db5f
Juraj Veverka <juraj.veverka@pantheon.tech>
2018-10-04 22:11:07 +0200

    POST mapping response content-type set to application/json

--- spring/springboot/src/main/java/itx/examples/springboot/demo/rest/DataServiceRest.java
+++ spring/springboot/src/main/java/itx/examples/springboot/demo/rest/DataServiceRest.java
@@ -2,26 +2,31 @@ package itx.examples.springboot.demo.rest;
 
 import itx.examples.springboot.demo.dto.DataMessage;
 import itx.examples.springboot.demo.dto.SystemInfo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/data")
 public class DataServiceRest {
 
-    @GetMapping(path = "/info", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/info", produces = MediaType.APPLICATION_JSON_VALUE )
     public SystemInfo getSystemInfo() {
         return new SystemInfo("spring-demo", "1.0.0", System.currentTimeMillis());
     }
 
-    @PostMapping(path = "/message", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody DataMessage getDataMessage(@RequestBody DataMessage dataMessage) {
-        return new DataMessage(dataMessage.getData());
+    @PostMapping(path = "/message", consumes = MediaType.APPLICATION_JSON_VALUE )
+    public ResponseEntity<DataMessage> getDataMessage(@RequestBody DataMessage dataMessage) {
+        DataMessage responseMessage = new DataMessage(dataMessage.getData());
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<DataMessage>(responseMessage, responseHeaders, HttpStatus.OK);
     }
 
 }
