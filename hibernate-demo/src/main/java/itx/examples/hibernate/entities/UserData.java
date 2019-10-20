1d5a18a8034a8c81fdba75c0b5c9f45445cacf89
Juraj Veverka <juraj.veverka@inter-net.sk>
2019-10-20 20:12:00 +0200

    removed import *

--- hibernate-demo/src/main/java/itx/examples/hibernate/entities/UserData.java
+++ hibernate-demo/src/main/java/itx/examples/hibernate/entities/UserData.java
@@ -1,6 +1,11 @@
 package itx.examples.hibernate.entities;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "userdata")
