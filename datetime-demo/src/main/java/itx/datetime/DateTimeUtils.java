a3729476018022698030aa0aeac890c9f5fcc245
Juraj Veverka <gergej123@gmail.com>
2021-03-13 17:08:15 +0100

    created datetime demo

---
+++ datetime-demo/src/main/java/itx/datetime/DateTimeUtils.java
@@ -0,0 +1,42 @@
+package itx.datetime;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public final  class DateTimeUtils {
+
+    private DateTimeUtils() {
+    }
+
+    public static Long getUtcTimeStamp() {
+        //System.currentTimeMillis();
+        return Instant.now().toEpochMilli();
+    }
+
+    public static Date getUtcDate() {
+        return Date.from(Instant.now());
+    }
+
+    public static Date getUtcDate(Long epochTime) {
+        return Date.from(Instant.ofEpochMilli(epochTime));
+    }
+
+    public static LocalDateTime getUtcLocalDateTime() {
+        return LocalDateTime.ofInstant(Instant.now(), ZoneOffset.UTC);
+    }
+
+    public static LocalDateTime getUtcLocalDateTime(Long epochTime) {
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(epochTime), ZoneOffset.UTC);
+    }
+
+    public static Instant getUtcInstant() {
+        return Instant.now();
+    }
+
+    public static Instant getUtcInstant(Long epochTime) {
+        return Instant.ofEpochMilli(epochTime);
+    }
+
+}
---
+++ datetime-demo/src/test/java/itx/datetime/DateTimeUtilsTests.java
@@ -0,0 +1,42 @@
+package itx.datetime;
+
+import org.testng.annotations.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import static itx.datetime.DateTimeUtils.getUtcDate;
+import static itx.datetime.DateTimeUtils.getUtcInstant;
+import static itx.datetime.DateTimeUtils.getUtcLocalDateTime;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class DateTimeUtilsTests {
+
+    @Test
+    void testingDate() {
+        Date originalDate = getUtcDate();
+        Long epochMillis = originalDate.getTime();
+        Date createdDate = getUtcDate(epochMillis);
+        assertEquals(originalDate, createdDate);
+        assertEquals(epochMillis, Long.valueOf(createdDate.getTime()));
+    }
+
+    @Test
+    void testLocalDateTime() {
+        LocalDateTime originalLocalDateTime = getUtcLocalDateTime();
+        Long epochMillis = originalLocalDateTime.atZone(ZoneOffset.UTC).toInstant().toEpochMilli();
+        LocalDateTime createdLocalDateTime = getUtcLocalDateTime(epochMillis);
+        assertEquals(originalLocalDateTime.toEpochSecond(ZoneOffset.UTC), createdLocalDateTime.toEpochSecond(ZoneOffset.UTC));
+    }
+
+    @Test
+    void testInstant() {
+        Instant originalInstant = getUtcInstant();
+        Long epochMillis = originalInstant.toEpochMilli();
+        Instant createdInstant = getUtcInstant(epochMillis);
+        assertEquals(originalInstant.toEpochMilli(), createdInstant.toEpochMilli());
+    }
+
+}
